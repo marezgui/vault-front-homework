8621376290a95a0a160b6a410618966cd73fd738
Malek Rezgui <m.rezgui@outlook.fr>
2023-04-23 19:14:42 +0200

    feat(ui): no result found indication

--- packages/ui/NotificationList/NotificationList.tsx
+++ packages/ui/NotificationList/NotificationList.tsx
@@ -5,10 +5,16 @@ type NotificationListProps = {
   notifications: Notifications;
 };
 
-export const NotificationList = ({ notifications }: NotificationListProps) => (
-  <div className="bg-white divide-y divide-gray-200 shadow rounded-lg overflow-hidden mt-4">
-    {notifications.map((notification) => (
-      <NotificationItem key={notification.id} {...notification} />
-    ))}
-  </div>
-);
+export const NotificationList = ({ notifications }: NotificationListProps) => {
+  if (notifications.length === 0) {
+    return <div className="text-center text-gray-500 mt-8"> No notifications found</div>;
+  }
+
+  return (
+    <div className="bg-white divide-y divide-gray-200 shadow rounded-lg overflow-hidden mt-4">
+      {notifications.map((notification) => (
+        <NotificationItem key={notification.id} {...notification} />
+      ))}
+    </div>
+  );
+};
